85cd7bd6288a1f12340674cfb5b4e4ef9ace642d
Nat Tuck <nat@ferrus.net>
2023-10-01 19:57:40 -0400

    pull in asdf in scripts

--- bin/build.sh
+++ bin/build.sh
@@ -3,6 +3,10 @@
 export RAILS_ENV=production
 export NODE_ENV=production
 
+if [[ -e "$HOME/.asdf/asdf.sh" ]]; then
+    . "$HOME/.asdf/asdf.sh"
+fi
+
 yarn
 
 bin/bundle install
--- bin/start.sh
+++ bin/start.sh
@@ -2,4 +2,8 @@
 
 export RAILS_ENV=production
 
+if [[ -e "$HOME/.asdf/asdf.sh" ]]; then
+    . "$HOME/.asdf/asdf.sh"
+fi
+
 bundle exec puma -v -b "tcp://127.0.0.1:3077"
