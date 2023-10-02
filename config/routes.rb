c1bccba3eb63369b39398d543a6125cf29c98bc4
Nat Tuck <nat@ferrus.net>
2023-10-01 20:08:08 -0400

    Add an index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  resources :jokes
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "page#index"
+
+  resources :jokes
 end
